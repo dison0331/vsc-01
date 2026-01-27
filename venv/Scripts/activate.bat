234c06554e4b5b6b5c653015546f90e4a44803a9
class_computer <139662128+dison0331@users.noreply.github.com>
2026-01-27 21:18:54 +0800

    2026.01.27
    02

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=G:\vsc-dm\venv"
+set "VIRTUAL_ENV=g:\GitHub\vsc-01\venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
